d13eeaf48c3d6301c9754d95f7b07a7047476161
Susmit Mukherjee <75859651+SusmitM@users.noreply.github.com>
2023-11-18 13:06:52 +0530

    Monitoring Data added

---
+++ src/Data/MonitoringData.js
@@ -0,0 +1,13 @@
+export const MonitoringData=[
+    {userName:"Sam Altman",userEmail:"samaltman123@gmaiprashanth@onjuno.com",riskLevel:"Medium",triggerReason:"IP Change",queueFor:4,dateAdded:"12 Oct, 2023",actionReason:"Flagged",timeToClose:14,actionTakenByName:"Neil",actionTakenByEmail:"neil@onjuno.com",reviewStatus:"Yes",reviewDate:"23 Aug 2023"},
+
+    {userName:"Sameer Choubey",userEmail:"sameerchoubey123@gmail.com",riskLevel:"High",triggerReason:"FIFO",queueFor:4,dateAdded:"12 Oct, 2023",actionReason:"Closed",timeToClose:14,actionTakenByName:"Pratik",actionTakenByEmail:"pratik@onjuno.com",reviewStatus:"No"},
+
+    {userName:"Adarsh Panikkar",userEmail:"adarsh@onjuno.com",riskLevel:"Low",triggerReason:"IP Change",queueFor:5,dateAdded:"12 Oct, 2023",actionReason:"Cleared",timeToClose:15,actionTakenByName:"Prashanth",actionTakenByEmail:"prashanth@onjuno.com",reviewStatus:"No"},
+
+    {userName:"Pratik Shetty",userEmail:"pratik3@gmail.com",riskLevel:"High",triggerReason:"FIFO",queueFor:5,dateAdded:"12 Oct, 2023",actionReason:"SOI requested",timeToClose:15,actionTakenByName:"Rasleen Kaur",actionTakenByEmail:"rasleen@onjuno.com",reviewStatus:"Yes",reviewDate:"12 Sep 2023"},
+
+    {userName:"Elon Musk",userEmail:"elon@twitterceo.com",riskLevel:"Low",triggerReason:"FIFO",queueFor:5,dateAdded:"12 Oct, 2023",actionReason:"Flagged",timeToClose:15,actionTakenByName:"Pratik Shetty",actionTakenByEmail:"pratik@onjuno.com",reviewStatus:"Yes",reviewDate:"12 Sep 2023"},
+
+    {userName:"Trina Kundu",userEmail:"trina@onjuno.com",riskLevel:"Low",triggerReason:"FIFO",queueFor:5,dateAdded:"12 Oct, 2023",actionReason:"Closed",timeToClose:15,actionTakenByName:"Varun Deshpande",actionTakenByEmail:"varun@onjuno.com",reviewStatus:"Yes",reviewDate:"12 Sep 2023"},
+]
