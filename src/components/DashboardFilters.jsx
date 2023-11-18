6bce94059fc4db6ce8a9fadec10bca07f58e129e
Susmit Mukherjee <75859651+SusmitM@users.noreply.github.com>
2023-11-18 22:27:54 +0530

    Feat: Risk Value Filter Added

--- src/components/DashboardFilters.jsx
+++ src/components/DashboardFilters.jsx
@@ -3,19 +3,37 @@ import SearchIcon from "../assets/SearchIcon.svg";
 import { IoIosArrowUp } from "react-icons/io";
 import { IoIosArrowDown } from "react-icons/io";
 
-const DashboardFilters = ({ searchInput, setSearchInput, selectedTab,triggerFilterValue, setTriggerFilterValue,actionFilterValue, setActionFilterValue }) => {
-  
-  const [openTriggerReason,setOpenTriggerReason]=useState(false);  
-  const [openActionReason,setOpenActionReason]=useState(false);  
+//Dropdown Values
+const TriggerReasonValues = [
+  "Hard flag",
+  "Temp flag",
+  "Restructed unflag",
+  "Un flag",
+  "Reviewed",
+  "FIFO",
+  "IP Change",
+];
+const ActionReasonValues = ["Flagged", "Closed", "Cleared", "SOI requested"];
+const RiskLevelValues = ["Low", "Medium", "High"];
 
-
-    const TriggerReasonValues=["Hard flag","Temp flag","Restructed unflag","Un flag","Reviewed","FIFO","IP Change"];
-
-    const ActionReasonValues=["Flagged","Closed","Cleared","SOI requested"]
+const DashboardFilters = ({
+  searchInput,
+  setSearchInput,
+  selectedTab,
+  triggerFilterValue,
+  setTriggerFilterValue,
+  actionFilterValue,
+  setActionFilterValue,
+  selectedRiskValue,
+  setSelectedRiskValue,
+}) => {
+  // states to manage opening and closing of dropdowns
+  const [openTriggerReason, setOpenTriggerReason] = useState(false);
+  const [openActionReason, setOpenActionReason] = useState(false);
+  const [openRiskValue, setOpenRiskValue] = useState(false);
 
   return (
-    <div className="mt-9 flex flex-col sm:flex-row gap-3">
-
+    <div className="mt-9 w-fit flex flex-col sm:flex-row gap-3 justify-center align-center m-auto">
       {/* Search Bar */}
       <div className="relative">
         <input
@@ -30,58 +48,126 @@ const DashboardFilters = ({ searchInput, setSearchInput, selectedTab,triggerFilt
         </span>
       </div>
 
-      {/* Trigger Reason Filter*/}
+      <div className="flex flex-col sm:flex-row gap-3 sm:gap-0">
+          {/* Trigger Reason Filter*/}
 
       {selectedTab === "Pending" && (
         <div className="relative flex flex-col items-center rounded-lg mx-3 px-2 w-44">
-            <button className="bg-gray-100 text-slate-500 p-2 w-full items-center justify-between font-medium text-sm rounded-lg flex gap-1.5" onClick={()=>setOpenTriggerReason(prev=>!prev)}>
-                {triggerFilterValue?triggerFilterValue:"Trigger Reason"} {openTriggerReason?<IoIosArrowUp/>:<IoIosArrowDown/>}
-            </button>
 
-        {openTriggerReason&&
-        <div className="bg-white absolute top-10 p-2 text-slate-500 font-medium text-sm w-full border border-slate-200 rounded-lg ">
+          <button
+            className="bg-gray-100 text-slate-500 p-2 w-full items-center justify-between font-medium text-sm rounded-lg flex gap-1.5"
+            onClick={() => setOpenTriggerReason((prev) => !prev)}
+          >
+            {triggerFilterValue ? triggerFilterValue : "Trigger Reason"}{" "}
+            {openTriggerReason ? <IoIosArrowUp /> : <IoIosArrowDown />}
+          </button>
 
-          <div onClick={()=>{setTriggerFilterValue(null);setOpenTriggerReason(false)}} className="p-1.5 cursor-pointer hover:bg-indigo-100 hover:text-black rounded-lg">
+          {openTriggerReason && (
+            <div className="bg-white z-10 absolute top-10 p-2 text-slate-500 font-medium text-sm w-full border border-slate-200 rounded-lg ">
+              <div
+                onClick={() => {
+                  setTriggerFilterValue(null);
+                  setOpenTriggerReason(false);
+                }}
+                className="p-1.5  cursor-pointer hover:bg-indigo-100 hover:text-black rounded-lg"
+              >
                 <h3>Reset</h3>
               </div>
-          {
-            TriggerReasonValues.map((value,index)=>(
-              <div onClick={()=>{setTriggerFilterValue(value);setOpenTriggerReason(false)}} className="p-1.5 cursor-pointer hover:bg-indigo-100 hover:text-black rounded-lg" key={index}>
-                <h3>{value}</h3>
-              </div>
-            ))
-          }
-          </div>}
-
+              {TriggerReasonValues.map((value, index) => (
+                <div
+                  onClick={() => {
+                    setTriggerFilterValue(value);
+                    setOpenTriggerReason(false);
+                  }}
+                  className="p-1.5 cursor-pointer hover:bg-indigo-100 hover:text-black rounded-lg"
+                  key={index}
+                >
+                  <h3>{value}</h3>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
-        
       )}
       {/* Action Reason Filter*/}
 
       {selectedTab === "Completed" && (
         <div className="relative flex flex-col items-center rounded-lg mx-3 px-2 w-44">
-            <button className="bg-gray-100 text-slate-500 p-2 w-full items-center justify-between font-medium text-sm rounded-lg flex gap-1.5" onClick={()=>setOpenActionReason(prev=>!prev)}>
-                {actionFilterValue?actionFilterValue:"Action Reason"} {openActionReason?<IoIosArrowUp/>:<IoIosArrowDown/>}
-            </button>
+          <button
+            className="bg-gray-100 text-slate-500 p-2 w-full items-center justify-between font-medium text-sm rounded-lg flex gap-1.5"
+            onClick={() => setOpenActionReason((prev) => !prev)}
+          >
+            {actionFilterValue ? actionFilterValue : "Action Reason"}{" "}
+            {openActionReason ? <IoIosArrowUp /> : <IoIosArrowDown />}
+          </button>
 
-        {openActionReason&&
-        <div className="bg-white absolute top-10 p-2 text-slate-500 font-medium text-sm w-full border border-slate-200 rounded-lg ">
-
-          <div onClick={()=>{setActionFilterValue(null);setOpenActionReason(false)}} className="p-1.5 cursor-pointer hover:bg-indigo-100 hover:text-black rounded-lg">
+          {openActionReason && (
+            <div className="bg-white absolute z-10 top-10 p-2 text-slate-500 font-medium text-sm w-full border border-slate-200 rounded-lg ">
+              <div
+                onClick={() => {
+                  setActionFilterValue(null);
+                  setOpenActionReason(false);
+                }}
+                className="p-1.5 cursor-pointer hover:bg-indigo-100 hover:text-black rounded-lg"
+              >
                 <h3>Reset</h3>
               </div>
-          {
-           ActionReasonValues.map((value,index)=>(
-              <div onClick={()=>{setActionFilterValue(value);setOpenActionReason(false)}} className="p-1.5 cursor-pointer hover:bg-indigo-100 hover:text-black rounded-lg" key={index}>
+              {ActionReasonValues.map((value, index) => (
+                <div
+                  onClick={() => {
+                    setActionFilterValue(value);
+                    setOpenActionReason(false);
+                  }}
+                  className="p-1.5 cursor-pointer hover:bg-indigo-100 hover:text-black rounded-lg"
+                  key={index}
+                >
+                  <h3>{value}</h3>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      )}
+
+      {/* Risk Reason Filter*/}
+      <div className="relative flex flex-col items-center rounded-lg mx-3 px-2 w-44">
+        <button
+          className="bg-gray-100 z-0 text-slate-500 p-2 w-full items-center justify-between font-medium text-sm rounded-lg flex gap-1.5"
+          onClick={() => setOpenRiskValue((prev) => !prev)}
+        >
+          {selectedRiskValue ? selectedRiskValue : "Risk level"}{" "}
+          {openActionReason ? <IoIosArrowUp /> : <IoIosArrowDown />}
+        </button>
+
+        {openRiskValue && (
+          <div className="bg-white z-10 absolute top-10 p-2 text-slate-500 font-medium text-sm w-full border border-slate-200 rounded-lg ">
+            <div
+              onClick={() => {
+                setSelectedRiskValue(null);
+                setOpenRiskValue(false);
+              }}
+              className="p-1.5 cursor-pointer hover:bg-indigo-100 hover:text-black rounded-lg"
+            >
+              <h3>Reset</h3>
+            </div>
+            {RiskLevelValues.map((value, index) => (
+              <div
+                onClick={() => {
+                  setSelectedRiskValue(value);
+                  setOpenRiskValue(false);
+                }}
+                className="p-1.5 cursor-pointer hover:bg-indigo-100 hover:text-black rounded-lg"
+                key={index}
+              >
                 <h3>{value}</h3>
               </div>
-            ))
-          }
-          </div>}
+            ))}
+          </div>
+        )}
+      </div>
+      </div>
 
-        </div>
-        
-      )}
+    
     </div>
   );
 };
--- src/components/MonitoringContent.jsx
+++ src/components/MonitoringContent.jsx
@@ -9,16 +9,19 @@ const MonitoringContent = () => {
 
   const [selectedTab, setSelectedTab] = useState("Pending");
   const [showModal,setShowModal]=useState(false);
+  
+  //states to manage Filters
   const[searchInput,setSearchInput]=useState("");
-
   const [triggerFilterValue, setTriggerFilterValue] = useState(null);
   const [actionFilterValue, setActionFilterValue] = useState(null);
+  const [selectedRiskValue,setSelectedRiskValue]=useState(null);
 
   
   const handelTabChange=(newTabValue)=>{
     setSelectedTab(newTabValue);
     setTriggerFilterValue(null);
     setActionFilterValue(null);
+    setSelectedRiskValue(null);
   }
   const openModal=()=>{
     setShowModal(true)
@@ -36,23 +39,26 @@ const MonitoringContent = () => {
       finalResult=searchFilteredData
     }
     if(triggerFilterValue){
-      finalResult=finalResult.filter(({triggerReasonValue})=>triggerReasonValue===triggerFilterValue);
+      finalResult=finalResult.filter(({triggerReason})=>triggerReason===triggerFilterValue);
        
     }
     if(actionFilterValue){
       finalResult=finalResult.filter(({actionReason})=>actionReason===actionFilterValue);
         
     }
+    if(selectedRiskValue){
+      finalResult=finalResult.filter(({riskLevel})=>riskLevel===selectedRiskValue);
+    }
 
     return finalResult
   }
 
   return (
-    <div className="sm:mx-12 mx-2 w-full">
+    <div className="sm:mx-12 mx-2 w-full overflow-x-hidden">
       <p className="text-3xl font-medium sm:mt-12 mt-4">Monitoring</p>
     <DashboardHeader selectedTab={selectedTab} handelTabChange={handelTabChange} openModal={openModal}/>
 
-    <DashboardFilters searchInput={searchInput} setSearchInput={setSearchInput} selectedTab={selectedTab} triggerFilterValue={triggerFilterValue} setTriggerFilterValue={setTriggerFilterValue} actionFilterValue={actionFilterValue} setActionFilterValue={setActionFilterValue}/>
+    <DashboardFilters searchInput={searchInput} setSearchInput={setSearchInput} selectedTab={selectedTab} triggerFilterValue={triggerFilterValue} setTriggerFilterValue={setTriggerFilterValue} actionFilterValue={actionFilterValue} setActionFilterValue={setActionFilterValue} selectedRiskValue={selectedRiskValue} setSelectedRiskValue={setSelectedRiskValue}/>
 
     <DashboardTable selectedTab={selectedTab} filteredData={filteredData}/>
 
